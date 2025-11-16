f5e631bc1d27f24f490332a3a04135a5c9477ef4
sharad-77 <chavdasharad77@gmail.com>
2025-11-16 19:25:16 +0530

    cors configuration changed

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 
+const FRONTENDURL = process.env.FRONTEND_URL;
+
 const app = express();
+
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: FRONTENDURL,
   credentials: true
 }));
 
